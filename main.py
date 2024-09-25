30b6e968bfd456295132bb2a5656a15b3eeb7295
pronan <pronan@loyola.edu>
2024-09-25 16:04:11 -0400

    beginning comments

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-# Programmers:
-# Course:
-# Due Date:
-# Lab Assignment:
-# Problem Statement:
-# Data In:
-# Data Out:
-# Credits:
+# Programmers:Paige and Theresa
+# Course:CS*151
+# Due Date:10/02/2024
+# Lab Assignment:Lab3
+# Problem Statement:What type of speed does a ski jumper need to achieve on the ramp to make
+# a good distance on their jump?
+# Data In: Hill type and jumpers speed
+# Data Out: The time in the air, distance traveled, points earned, and a statement on how well they did
+# Credits: Class resources
